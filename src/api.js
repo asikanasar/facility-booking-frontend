f3459286e5d549a8a40baecc4afc960b66264c8a
asikanasar <23suca30@tcarts.in>
2026-03-14 18:46:45 +0530

    azure

--- src/api.js
+++ src/api.js
@@ -4,7 +4,10 @@ const API_BASE_URL =
   process.env.REACT_APP_API_URL ||
   "https://facility-booking-backend-b4fweaehged4fxda.southeastasia-01.azurewebsites.net";
 
-const baseURL = `${API_BASE_URL.replace(/\/$/, "")}/api`;
+const normalizedBase = API_BASE_URL.replace(/\/$/, "");
+const baseURL = normalizedBase.endsWith("/api")
+  ? normalizedBase
+  : `${normalizedBase}/api`;
 
 const api = axios.create({
   baseURL,
